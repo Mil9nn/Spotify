2779328c6258f791f1d0e2a12753ab2e2e17bfd0
Milan Singh <100085851+Mil9nn@users.noreply.github.com>
2025-02-04 19:12:33 +0530

    did some trial and error to fetch songs properly without hardcoding the url

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -79,7 +79,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -94,7 +94,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").splice(-1)[0];
             // Get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
 
             cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
@@ -193,6 +193,7 @@ async function main() {
         if (e.target.src.includes("volume.svg")) {
             e.target.src = e.target.src.replace("volume.svg", "mute.svg");
             currentSong.volume = 0;
+            document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
         }
         else {
             e.target.src = e.target.src.replace("mute.svg", "volume.svg");
